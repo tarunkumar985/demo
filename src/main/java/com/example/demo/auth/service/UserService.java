e990b7449c0213e9bf5392dfb23e7e28790ed41c
tarunkumar985 <tarunkumar985@gmail.com>
2025-04-11 13:58:55 +0530

    removed unwanted lines

--- src/main/java/com/example/demo/auth/service/UserService.java
+++ src/main/java/com/example/demo/auth/service/UserService.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
